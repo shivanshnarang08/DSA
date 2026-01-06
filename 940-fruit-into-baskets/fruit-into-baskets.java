4653d35948868e945a2bc9cc21bdaa81ffee8739
shivanshnarang08 <155061209+shivanshnarang08@users.noreply.github.com>
2026-01-07 03:07:48 +0530

    Time: 10 ms (91.33%) | Memory: 74.4 MB (5.29%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.java
+++ 940-fruit-into-baskets/fruit-into-baskets.java
@@ -1,21 +1,18 @@
 class Solution {
     public int totalFruit(int[] fruits) {
-        int l = 0, maxLen = 0;
-        int[] count = new int[100001]; 
-        int distinct = 0;
-
-        for (int r = 0; r < fruits.length; r++) {
-            if (count[fruits[r]] == 0) distinct++;
+        int l=0,maxlength=0;
+        int distinct=0;
+        int[] count=new int[100001];
+        for(int r=0;r<fruits.length;r++){
+            if(count[fruits[r]]==0) distinct++;
             count[fruits[r]]++;
-
-            if (distinct > 2) {
+            if(distinct>2){
                 count[fruits[l]]--;
-                if (count[fruits[l]] == 0) distinct--;
+                if(count[fruits[l]]==0) distinct--;
                 l++;
             }
-
-            maxLen = Math.max(maxLen, r - l + 1);
+            maxlength=Math.max(maxlength,r-l+1);
         }
-        return maxLen;
+        return maxlength;
     }
 }
